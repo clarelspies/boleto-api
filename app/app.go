08c005f45574c0963b4c0133c52db123e328f43e
Paulo Lima <paulolimarb@gmail.com>
2019-10-22 12:15:24 -0300

    decrease time sleep

--- app/app.go
+++ app/app.go
@@ -60,7 +60,7 @@ func installCertificates() {
 	l := log.CreateLog()
 	var err error
 
-	time.Sleep(5 * time.Minute)
+	time.Sleep(1 * time.Minute)
 	if config.Get().MockMode == false && config.Get().EnableFileServerCertificate == false {
 		err = certificate.InstanceStoreCertificatesFromAzureVault(config.Get().VaultName, config.Get().CertificateICPName, config.Get().CertificateSSLName)
 		fmt.Println("Fim: " + time.Now().String())
